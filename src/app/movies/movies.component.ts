61bd2c27217351b90118c55bfeaba125e431f4c8
Sam <36157647+oxedom@users.noreply.github.com>
2022-02-23 10:47:41 +0200

    showing all movies added skema

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AxiosService } from '../axios.service';
 
 @Component({
   selector: 'app-movies',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoviesComponent implements OnInit {
 
-  constructor() { }
+  movies : any[] = []
+  constructor(private axios : AxiosService) { }
 
   ngOnInit(): void {
+    this.axios.getMovies()
+    .subscribe( (data : any) => { this.movies = data.splice(0,5)})
   }
 
 }
